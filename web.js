085c39720dfedd2b88ade8346900201b031e4550
チャーリー <eternalcharlie@wirelessprvnat-172-16-254-173.near.illinois.edu>
2013-11-09 21:40:39 -0600

    socket io bootstrap

--- web.js
+++ web.js
@@ -1,4 +1,12 @@
-var io = require('socket.io').listen(8000);
+var app = require('express')()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
+
+server.listen(8000);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
 
 io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
