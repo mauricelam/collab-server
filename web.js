8063f53bade48ab4508f27e31c2c373fa39120f3
チャーリー <eternalcharlie@wirelessprvnat-172-16-254-173.near.illinois.edu>
2013-11-09 21:44:24 -0600

    random

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = require('express')()
 server.listen(8000);
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+  res.sendfile('index.html');
 });
 
 io.sockets.on('connection', function (socket) {
